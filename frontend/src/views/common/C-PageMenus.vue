733505159ee4daacb2048e09135b3872a447ef68
mshe <666666666@666666666.666666666>
2024-11-13 17:57:55 +0800

    feat(icon): 新增图标选择功能并优化菜单图标显示
    - 新增 CIconSelect 组件用于选择图标
    - 在菜单添加和编辑页面集成图标选择功能
    -优化菜单列表和详情页面的图标

--- frontend/src/views/common/C-PageMenus.vue
+++ frontend/src/views/common/C-PageMenus.vue
@@ -3,57 +3,94 @@
     <a-menu mode="inline" :theme="theme" style="height: 100%"
             :default-open-keys="OPEN_MENU_IDS"
             :defaultSelectedKeys="SELECT_MENU_IDS">
-      <!-- 判断一级菜单是否包含children -->
-      <a-sub-menu :key="item.id" v-for="item in menus" v-if="item.children && item.children.length">
-        <span slot="title">
-            <svg class="iconfont" aria-hidden="true">
-            <use :xlink:href="'#icon-'+item.icon"></use>
-          </svg>
+      <template v-for="item in menus">
+        <a-menu-item v-if="!item.children" :key="item.key">
+           <svg class="iconfont" aria-hidden="true">
+                <use :xlink:href="'#icon-'+item.icon"></use>
+              </svg>
           <span>{{ item.name }}</span>
-        </span>
+        </a-menu-item>
 
-        <!-- 如果包含子children则遍历生成二级菜单 -->
-        <a-sub-menu :key="item.id" v-if="item.children && item.children.length" v-for="item in item.children">
-          <span slot="title">
+        <template v-else>
+          <a-sub-menu :key="item.id">
+            <span slot="title">
+               <svg class="iconfont" aria-hidden="true">
+                <use :xlink:href="'#icon-'+item.icon"></use>
+              </svg>
+              <span>{{ item.name }}</span>
+            </span>
+            <a-menu-item :key="item.key" v-for="item in item.children">
               <svg class="iconfont" aria-hidden="true">
                 <use :xlink:href="'#icon-'+item.icon"></use>
               </svg>
-            <span>{{ item.name }}</span>
-          </span>
-          <!--    看起来没什么用,先注释掉,后续确认即可删除      -->
-          <!--          <a-menu-item :key="item.id" v-for="item in item.children" @click="click(item)">-->
-          <!--            <svg class="iconfont" aria-hidden="true">-->
-          <!--              <use :xlink:href="'#icon-'+item.icon"></use>-->
-          <!--            </svg>-->
-          <!--            <span>{{ item.name }}</span>-->
-          <!--            <span>{{ item.icon }}</span>-->
-          <!--          </a-menu-item>-->
-        </a-sub-menu>
-
-        <!-- 生成二级菜单 -->
-        <a-menu-item :key="item.id" v-else @click="click(item)">
-          <svg class="iconfont" aria-hidden="true">
-            <use :xlink:href="'#icon-'+item.icon"></use>
-          </svg>
-          <span>{{ item.name }}</span>
-        </a-menu-item>
-      </a-sub-menu>
-
-      <!-- 如果不包含children则直接生成一级菜单 -->
-      <a-menu-item :key="item.id" v-else @click="click(item)">
-        <svg class="iconfont" aria-hidden="true">
-          <use :xlink:href="'#icon-'+item.icon"></use>
-        </svg>
-        <span>{{ item.name }}</span>
-      </a-menu-item>
+              <span>{{ item.name }}</span>
+            </a-menu-item>
+          </a-sub-menu>
+        </template>
+      </template>
     </a-menu>
   </a-layout-sider>
 </template>
 
 <script>
+// recommend use functional component
+// <template functional>
+//   <a-sub-menu :key="props.menuInfo.key">
+//     <span slot="title">
+//       <a-icon type="mail" /><span>{{ props.menuInfo.title }}</span>
+//     </span>
+//     <template v-for="item in props.menuInfo.children">
+//       <a-menu-item v-if="!item.children" :key="item.key">
+//         <a-icon type="pie-chart" />
+//         <span>{{ item.title }}</span>
+//       </a-menu-item>
+//       <sub-menu v-else :key="item.key" :menu-info="item" />
+//     </template>
+//   </a-sub-menu>
+// </template>
+// export default {
+//   props: ['menuInfo'],
+// };
+import {Menu} from 'ant-design-vue';
+
+const SubMenu = {
+  template: `
+    <p>1213</p>
+    <!--    <a-sub-menu :key="menuInfo.id" v-bind="$props" v-on="$listeners">-->
+    <!--      <p>{{ menuInfo }}</p>-->
+    <!--        <span slot="title">-->
+    <!--          <a-icon type="mail"/><span>{{ menuInfo.name }}</span>-->
+    <!--        </span>-->
+    <!--      <template v-for="item in menuInfo.children">-->
+    <!--        <a-menu-item v-if="!item.children" :key="item.key">-->
+    <!--          <a-icon type="pie-chart"/>-->
+    <!--          <span>{{ item.name }}</span>-->
+    <!--        </a-menu-item>-->
+    <!--        <sub-menu v-else :key="item.key" :menu-info="item"/>-->
+    <!--      </template>-->
+    <!--    </a-sub-menu>-->
+  `,
+  name: 'SubMenu',
+  // must add isSubMenu: true
+  isSubMenu: true,
+  props: {
+    ...Menu.SubMenu.props,
+    // Cannot overlap with properties within Menu.SubMenu.props
+    menuInfo: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  mounted() {
+    console.log(this.menuInfo.name)
+    console.log(SubMenu)
+  }
+};
 
 export default {
-  name: 'C-PageMenus',
+  components: {
+    'sub-menu': SubMenu,
+  },
   props: {
     theme: {
       type: String,
@@ -73,25 +110,8 @@ export default {
     return {
       OPEN_MENU_IDS: [localStorage.getItem('OPEN_MENU_IDS')],
       SELECT_MENU_IDS: [localStorage.getItem('SELECT_MENU_IDS')],
-    }
+    };
   },
-  beforeCreate() {
-  },
-  activated() {
-  },
-  mounted() {
-    console.log(this.OPEN_MENU_IDS)
-  },
-  methods: {
-    click(e) {
-      localStorage.setItem('OPEN_MENU_IDS', e.pid)
-      localStorage.setItem('SELECT_MENU_IDS', e.id)
-      this.$emit('clickMenu', e)
-    },
-  }
-}
+  methods: {},
+};
 </script>
-
-<style scoped lang="less">
-@import "~@public/color.less";
-</style>
